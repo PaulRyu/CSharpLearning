e832938d97e975c1149385b5221e38165f55ab1e
PaulRyu <pauliedho@gmail.com>
2022-09-29 08:01:07 -0500

    Add project files.

---
+++ ConsoleApp1/Characters/Warriors/Warrior.cs
@@ -0,0 +1,35 @@
+﻿namespace ConsoleApp1.Characters.Warriors
+{
+    public class Warrior
+    {
+        private int height;
+        private int weight;
+        private string name;
+
+        public int Height 
+        {
+            get
+            {
+                return height;
+            }
+
+            set
+            {
+                
+            }
+        }
+        public int Weight { get; set; }
+        public string Name { get; set; }
+
+        public Warrior(int height, int weight, string name)
+        {
+            Height = height;
+            Weight = weight;
+            Name = name;
+        }
+        public void Greetings(Warrior warrior)
+        {
+            Console.WriteLine($@"Greetings, {warrior.Name}!");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using ConsoleApp1.Characters.Warriors;
+
+public class EntryPoint
+{
+    static void Main()
+    {
+        Warrior theGoodGuy = new Warrior(10, 100, "Barnabas");
+
+        Warrior theBadGuy = new Warrior(100, 500, "Chad");
+
+        Console.WriteLine(theGoodGuy.Height);
+        Console.WriteLine(theBadGuy.Height);
+
+        theGoodGuy.Greetings(theBadGuy);
+        theBadGuy.Greetings(theGoodGuy);
+    }
+}
