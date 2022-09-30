b7dd0723e93964647d46bf925949fc65c75e8ad4
PaulRyu <pauliedho@gmail.com>
2022-09-30 09:10:37 -0500

    Just code

--- ConsoleApp1/Characters/Warriors/Warrior.cs
+++ ConsoleApp1/Characters/Warriors/Warrior.cs
@@ -4,8 +4,27 @@
     {
         private int height;
         private int weight;
+        private int age;
         private string name;
 
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value >= 18 && value <= 45)
+                {
+                    age = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException(string.Empty, $@"Improper age for Warrior: {name}");
+                }
+            }
+        }
         public int Height 
         {
             get
@@ -15,11 +34,31 @@
 
             set
             {
-                
+                height = value;
             }
         }
-        public int Weight { get; set; }
-        public string Name { get; set; }
+        public int Weight
+        {
+            get
+            {
+                return weight;
+            }
+            set
+            {
+                weight = value;
+            }
+        }
+        public string Name 
+        { 
+            get
+            {
+                return name;
+            } 
+            set
+            {
+                name = value;
+            }
+        }
 
         public Warrior(int height, int weight, string name)
         {
